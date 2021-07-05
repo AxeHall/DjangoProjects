9cc335301aaf73cf052867dea4abded3b4c87ffb
axehall <akjol.kadyrakunov@gmail.com>
2021-07-05 23:05:37 +0600

    We only added Jinja to the home page

--- solution/main/urls.py
+++ solution/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, AboutView, HomeView, NewsView, ServicesView
 
 urlpatterns = [
-    path("", MainView.as_view(), name="main_view"),
+    path("wrapper", MainView.as_view(), name="main_view"),
+    path("home/", HomeView.as_view(), name="main_home"),
+    path("about/", AboutView.as_view(), name="main_about"),
+    path("news/", NewsView.as_view(), name="main_news"),
+    path("services/", ServicesView.as_view(), name="main_services"),
 ]
--- solution/main/views.py
+++ solution/main/views.py
@@ -5,3 +5,26 @@ from django.views.generic import View
 class MainView(View):
     def get(self, request):
         return render(request, template_name="main/wrapper.html")
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, template_name="main/about.html")
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, template_name="main/home.html", 
+        context={
+            "home_image_path": "images/learn-img.jpg",
+            "home_title": "We are a Creative Digital Agency & Marketing Experts",
+            "home_theme": "Pure Workflow. No Limits. Boundless configuration with best performance, more process control & resource savings",
+            "home_description": "You don’t need to go to IT to create your own documents and spreadsheets. Why should processes be any different? Take charge of your processes, design your own forms and workflows, and bring automated efficiency to your team."
+        })
+
+class NewsView(View):
+    def get(self, request):
+        return render(request, template_name="main/news.html")
+
+class ServicesView(View):
+    def get(self, request):
+        return render(request, template_name="main/services.html")
+
